3f518ecd3a1bad08088d330d1f5053dc14a124d8
Alex El Adl <alexeladl@Alexs-MBP-3.fios-router.home>
2017-04-15 12:05:01 -0400

    stuff

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,41 +1,121 @@
- public class BinarySearchTree<T> extends BinaryTree{
+ public class BinarySearchTree<T> extends BinaryTree<T>{
+  protected Node<T> switch;
+
   public BinarySearchTree(){
   }
-  public boolean empty(){
-    return root == null;
-  }
-  public void clear(){
-    root = null;
-  }
+
   public boolean add(T item) {
     if(item == null)
-    throw new NullPointerException();
-
+      throw new NullPointerException();
     return addItem(item, root);
   }
-  private boolean addItem(T item, Node<T> thisRoot){
+
+  private boolean addItem(T item, Node<T> thisRoot) {
     if(thisRoot == null)
     {
       thisRoot = new Node<T>(item);
       return true;
     }
 
-    if(item.compareTo(thisRoot.getContent()) =< 0)
-    return addItem(item, thisRoot.left);
-
-    else
+    if(item.compareTo(thisRoot.contents) =< 0)
+      return addItem(item, thisRoot.left);
     return addItem(item, thisRoot.right);
   }
-  public void remove(T item) throws NullPointerException{
-    Node<T> n = this.get(item);
-    //Consider Null case
-    Node<T> x = n.left;
-    while(x.right != null){
-      x = x.right;
-    }
+
+  public boolean remove(T item) {
+    if (root == null)
+      return false;
+    else {
+    if (root.contents.equals(item)) {
+      Node<T> aux = new Node<T>(0);
+                       aux.setLeftChild(root);
+                       boolean result = root.remove(value, aux);
+                       root = aux.left;
+                       return result;
+                 } else {
+                       return root.remove(value, null);
+                 }
+           }
+     }
+
+  // public void remove(T item) throws NullPointerException {
+  //   if (!contains(item)) throw new NullPointerException();
+  //
+  //   while (true)
+  //   {
+  //     if (root.contents.equals(item))
+  //       break;
+  //
+  //     if(item.compareTo(root.contents) <= 0)
+  //       root = root.left;
+  //     root = root.right;
+  //   }
+  //   //at item to be removed
+  //   if (root.children().equals("B"))
+  //   {
+  //     switch = root.left;
+  //
+  //     while (switch.children().equals("R"))
+  //     {
+  //       switch = switch.right;
+  //     }
+  //     root.contents = switch.contents;
+  //     if (switch.children().equals)
+  //   }
+  //   else if (root.children().equals("L"))
+  //   {
+  //     switch =
+  //   }
+  //   else
+  //   {
+  //     root = null;
+  //   }
+  // }
+
+  public Node<T> get(T item) throws new NullPointerException {
 
   }
-  public boolean contains(T item){
-    return this.conatins(item, root);
+
+
+  public class Node<T>
+  {
+    private T contents;
+    public Node<T> left;
+    public Node<T> right;
+
+    public Node(T data)
+    {
+      contents = data;
+    }
+
+    public boolean remove(T item, Node<T> parent) {
+        if (item.compareTo(contents) < 0) {
+              if (left != null)
+                    return left.remove(value, this);
+              else
+                    return false;
+        } else if (item.compareTo(contents) > 0) {
+              if (right != null)
+                    return right.remove(value, this);
+              else
+                    return false;
+        } else {
+              if (left != null && right != null) {
+                    contents = right.minValue();
+                    right.remove(contents, this);
+              } else if (parent.left == this) {
+                    parent.left = (left != null) ? left : right;
+              } else if (parent.right == this) {
+                    parent.right = (left != null) ? left : right;
+              }
+              return true;
+        }
+    }
+
+    public int minValue() {
+        if (left == null)
+              return contents;
+        else
+              return left.minValue();
+    }
   }
-}
--- BinaryTree.java
+++ BinaryTree.java
@@ -1,107 +1,145 @@
 import java.util.LinkedList;
+import java.util.Queue;
 
 public class BinaryTree<T> {
   protected Node<T> root;
 
+  public BinaryTree(T item){
+    root = new Node<T>(item);
+  }
+
+  public boolean empty(){
+    return root == null;
+  }
+
+  public void clear(){
+    root = null;
+  }
+
+  public boolean add(T item){
+    LinkedList queue = new LinkedList();
+    queue.add(root);
+    while(queue.element().hasHowMany() == 2){
+      queue.add((Node)queue.element().left);
+      queue.add((Node)queue.element().right);
+      queue.removeFirst();
+    }
+
+    if (queue.element().left == null) {
+      queue.element().left = new Node(item);
+    }
+    else {
+      queue.element().right = new Node(item);
+    }
+  }
+
+  //TODO
+  public void remove(T item) throws NullPointerException{
+    if(!contains(item)) throw new NullPointerException("You Fool!!! Why???");
+
+
+
+  }
+
+  public boolean contains(T item){
+    return contains(item, root);
+  }
+
+  public boolean contains(T item, Node<T> n){
+    if (n.getContent() == item){
+      return true;
+    }
+    else if (n == null){
+      return false;
+    }
+    else{
+      return contains(item, n.left) || contains(item, n.right);
+    }
+    return false;
+  }
+
+  //TODO
+  public Node<T> get(T item) throws NullPointerException{
+    if (!contains(item)) throw new NullPointerException();
+
+    while (root.compareTo(item) )
+    while(queue.peek().hasHowMany() == 2){
+      queue.add(queue.peek().left);
+      queue.add(queue.peek().right);
+      if(queue.peek().equals(item))
+      {
+        return queue.peek()
+      }
+      queue.remove();
+    }
+
+    if (queue.peek().left.equals(item)) {
+      return queue.peek()
+    }
+    else if(queue.peek().right.equals(item)) {
+      return queue.peek()
+    }
+  }
+
+  //TODO
+  public Node<T> get(T item, Node<T> n){
+    if (n.getContent() = item){
+      return n;
+    }
+
+    else{
+
+    }
+  }
+
+  //TODO
+  public java.util.Iterator<T> breadthFirst(){
+
+  }
+
+  //TODO
+  public java.util.Iterator<T> depthFirst(){
+
+  }
+
   public class Node<T>{
-    private T content;
+    private T contents;
     public Node<T> left;
     public Node<T> right;
 
-      public Node(){}
-      public Node(T content){
-        this.content = content;
+      public Node(T data){
+        contents = data;
       }
-      public void setContent(T item){
-        this.content = item;
-      }
-      public T getContent(){
-        return this.content;
-      }
+
       public boolean addLeft(Node<T> n, T item){
         n.left = new Node<T>(item);
         return true;
       }
+
       public boolean addRight(Node<T> n, T item){
         n.right = new Node<T>(item);
         return true;
       }
-      public int hasHowMany(){
-        if(left == null && right == null) return 0;
-        else if(left == null ^ right == null) return 1;
-        else if(left != null && right != null) return 2;
-        return -1;
-      }
-    }
 
-    public BinaryTree(){
-    }
-    public boolean empty(){
-      return root == null;
-    }
-    public void clear(){
-      root = null;
-    }
-    public boolean add(T item){
-      LinkedList queue = new LinkedList();
-      queue.add(root);
-      while(queue.element().hasHowMany() == 2){
-        queue.add((Node) queue.element().left);
-        queue.add((Node)queue.element().right);
-        queue.removeFirst();
+      // returns true if both left and right are occupied
+      public int children(){
+        if (left != null && right != null)
+      		return 2;
+        if (left == null && right == null)
+          return 0;
+        return 1;
       }
-      if (queue.element().left == null){
-        queue.element().left = new Node(item);
-      }
-      else {
-        queue.element().right = new Node(item);
-      }
-    }
-    //TODO
-    public void remove(T item) throws NullPointerException{
-      if(!this.contains(item)) throw new NullPointerException("You Fool!!! Why???");
 
-    }
-    public boolean contains(T item){
-      return this.conatins(item, root);
-    }
-    public boolean contains(T item, Node<T> n){
-      if (n.getContent() == item){
-        return true;
-      }
-      else if (n == null){
+      public boolean hasLeftChild(){
+        if (left != null)
+        	return true;
         return false;
       }
-      else{
-        return this.contains(item, n.left) || this.contains(item, n.right);
-      }
-      return false;
-    }
-    //TODO
-    public Node<T> get(T item) throws NullPointerException{
-      if (!this.contains(item)){
-        throw new NullPointerException();
-      }
-      else {
 
-      }
-    }
-    //TODO
-    public Node<T> get(T item, Node<T> n){
-      if (n.getContent() = item){
-        return n;
-      }
-      if ()
-      else{
-
-      }
-    }
-    //TODO
-    public java.util.Iterator<T> breadthFirst(){
-
-    }
-    //TODO
-    public java.util.Iterator<T> depthFirst(){
-
+        public boolean hasRightChild(){
+        	if (right != null)
+        		return true;
+        	return false;
+        }
     }
   }
